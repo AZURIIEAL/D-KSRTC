ea50cae7a7aee9859f8062160a412bcc66f2e619
christina <christinajacob06@gmail.com>
2023-08-30 17:24:35 +0530

    vgds

---
+++ EFandApi/D-KSRTC/D-KSRTC/Models/BusType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace D_KSRTC.Models
+{
+    public class BusType
+    {
+        [Key]
+        public int TypeId { get; set; }
+
+        [Required]
+        public string TypeName { get; set; } = string.Empty;
+
+        [Required]
+        public float PDF { get; set; }
+    }
+}
