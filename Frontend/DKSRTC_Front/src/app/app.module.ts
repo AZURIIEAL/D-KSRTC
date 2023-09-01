d7d76b47752e1c4e5dc3d3fde8756e31660656e0
AZURIIEAL <abinbinu678@gmail.com>
2023-09-01 12:26:53 +0530

    Angular

---
+++ Frontend/DKSRTC_Front/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UserRedirectionPageComponent } from './Components/user-redirection-page/user-redirection-page.component';
+import { UserPageComponent } from './Components/user-page/user-page.component';
+import { AdminPageComponent } from './Components/admin-page/admin-page.component';
+import { AddNewUserComponent } from './Components/add-new-user/add-new-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserRedirectionPageComponent,
+    UserPageComponent,
+    AdminPageComponent,
+    AddNewUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
