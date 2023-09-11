474d4bb67a1da148cf7793dca4037d0454c78da1
AZURIIEAL <abinbinu678@gmail.com>
2023-09-11 12:06:24 +0530

    new updates

--- Frontend/DKSRTC_Front/src/app/Components/seat-booking/seat-booking.component.ts
+++ Frontend/DKSRTC_Front/src/app/Components/seat-booking/seat-booking.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import Validators and FormBuilder
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms'; // Import Validators and FormBuilder
 import { ISeatReservation } from 'src/app/Interfaces/ISeatReservation';
 
 @Component({
@@ -11,7 +11,7 @@ import { ISeatReservation } from 'src/app/Interfaces/ISeatReservation';
 export class SeatBookingComponent implements OnInit {
   selectedBusId!: number;
   seatRows: { selected: boolean }[][] = [];
-  maxSelections = 4;
+  maxSelections = 4
   selectedSeats: string[] = [];
   selectedSeatsData: ISeatReservation[] = [];
   seatReservationForm!: FormGroup; // Define a form group for seat reservations
@@ -23,13 +23,9 @@ export class SeatBookingComponent implements OnInit {
     // Initialize the form group with validators
   }
   CreateForm() {
+    const formArray = this.formBuilder.array([]);
     this.seatReservationForm = this.formBuilder.group({
-      FirstName: ['', [Validators.required, Validators.minLength(2)]],
-      LastName: ['', [Validators.required, Validators.minLength(2)]],
-      Age: ['', [Validators.required, Validators.min(0)]],
-      Gender: ['', Validators.required],
-      PhoneNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
-      Email: ['', [Validators.required, Validators.email]],
+      passengers: formArray
     });
   }
 
@@ -40,7 +36,7 @@ export class SeatBookingComponent implements OnInit {
       if (selectedBusIdParam) {
         this.selectedBusId = parseInt(selectedBusIdParam, 10);
       }
-    });
+    }); 
 
     // Initialize the seatRows (same as your previous code)
     for (let i = 0; i < 5; i++) {
@@ -72,6 +68,7 @@ export class SeatBookingComponent implements OnInit {
       const index = this.selectedSeats.indexOf(seat);
       if (index !== -1) {
         this.selectedSeats.splice(index, 1);
+        this.formArray.removeAt(index);
       }
       this.selectedSeatsData = this.selectedSeatsData.filter(
         (reservation) => reservation.SeatId !== seat
@@ -80,6 +77,7 @@ export class SeatBookingComponent implements OnInit {
       if (selectedCount >= this.maxSelections) {
         return;
       }
+      this.formArray.push(this.getControlGroup());
       this.seatRows[row][column].selected = true;
       this.selectedSeats.push(seat);
       this.selectedSeatsData.push({
@@ -93,4 +91,24 @@ export class SeatBookingComponent implements OnInit {
       });
     }
   }
-}
+
+  getPassengersGroup(index: number): FormGroup {
+    return this.formArray.at(index) as FormGroup;
+  }
+
+  private getControlGroup() {
+    return this.formBuilder.group({
+      Seat:['',[Validators.required]],
+      FirstName: ['', [Validators.required, Validators.minLength(2)]],
+      LastName: ['', [Validators.required, Validators.minLength(2)]],
+      Age: ['', [Validators.required, Validators.min(0)]],
+      Gender: ['', Validators.required],
+      PhoneNumber: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
+      Email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  get formArray() {
+    return this.seatReservationForm.get('passengers') as FormArray;
+  }
+} 
