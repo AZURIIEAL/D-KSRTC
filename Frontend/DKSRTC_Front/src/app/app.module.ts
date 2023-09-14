50bd703ca3d9d2f7b3ccb6a01eac1905309a8e21
christina <christinajacob06@gmail.com>
2023-09-14 15:56:41 +0530

    errorssolved

--- Frontend/DKSRTC_Front/src/app/app.module.ts
+++ Frontend/DKSRTC_Front/src/app/app.module.ts
@@ -24,7 +24,7 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { CheckOutComponent } from './Components/check-out/check-out.component';
 import { PaymentGatewayComponent } from './Components/payment-gateway/payment-gateway.component';
-// import { ViewTicketComponent } from './Components/view-ticket/view-ticket.component';
+import { ViewTicketComponent } from './Components/view-ticket/view-ticket.component';
 
 
 
@@ -43,6 +43,7 @@ import { PaymentGatewayComponent } from './Components/payment-gateway/payment-ga
     FooterComponent,
     NotFoundComponent,
     CheckOutComponent,
+    ViewTicketComponent
   ],
   imports: [
     MatSelectModule,
