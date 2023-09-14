b3b18a38cee11d6b7e4fcd292270b3d921d76d1a
christina <christinajacob06@gmail.com>
2023-09-14 15:34:17 +0530

    sfd

--- Frontend/DKSRTC_Front/src/app/Components/view-ticket/view-ticket.component.ts
+++ Frontend/DKSRTC_Front/src/app/Components/view-ticket/view-ticket.component.ts
@@ -1,54 +1,44 @@
-// import { Component } from '@angular/core';
-// import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-// import { IBookingDetails } from '../../Interfaces/ibooking-details';
-// import { TicketCancellationService } from '../../Services/ticket-cancellation.service';
-// import { IBookedBus } from 'src/app/Interfaces/ibooked-bus';
-// import { Ipassenger } from 'src/app/Interfaces/ipassenger';
-// import { Subscription } from 'rxjs';
-// import { BookedBusService } from 'src/app/Services/booked-bus.service';
-// import { BookingService } from 'src/app/Services/booking.service';
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { IBookingDetails } from '../../Interfaces/ibooking-details';
+import { TicketCancellationService } from '../../Services/ticket-cancellation.service';
+import { IBookedBus } from 'src/app/Interfaces/ibooked-bus';
+import { Ipassenger } from 'src/app/Interfaces/ipassenger';
+import { Subscription } from 'rxjs';
+import { BookedBusService } from 'src/app/Services/booked-bus.service';
+import { BookingService } from 'src/app/Services/booking.service';
+import { IPayload } from 'src/app/Interfaces/ipayload';
+import { IBookingPayload } from 'src/app/Interfaces/ibooking-payload';
 
-// @Component({
-//   selector: 'app-view-ticket',
-//   templateUrl: './view-ticket.component.html',
-//   styleUrls: ['./view-ticket.component.sass']
-// })
-// export class ViewTicketComponent {
-//   public bookedBus: Array<IBookedBus> = [];
-//   public newPassengerList: Array<Ipassenger> = [];
-//   subs?: Subscription;
-//   BookingId:number=0;
-//   total:number=0;
+@Component({
+  selector: 'app-view-ticket',
+  templateUrl: './view-ticket.component.html',
+  styleUrls: ['./view-ticket.component.sass']
+})
+export class ViewTicketComponent {
+  public bookedBus: Array<IPayload> = [];
+  public newPassengerList: Array<Ipassenger> = [];
+  subs?: Subscription;
+  BookingId:number=0;
+  total:number=0;
 
-//   constructor(private bookedBusService: BookedBusService,
-//     private passengerService:PassengerDetailsService,
-//     private router: Router,
-//     private bookingService: BookingService) {}
+  i:number=this.bookedBus[0].booking.userId;
 
-//     ngOnInit(){
-//       this.subs = this.bookedBusService.select.subscribe(
-//         (x: Array<IBookedBus>) => (this.bookedBus = x)
-//       );
+  constructor(private bookingService: BookingService) {}
 
-//       this.subs=this.passengerService.passenger.subscribe(
-//         (x:Array<Ipassenger>)=>{
-//           (this.newPassengerList=x)
-//         this.BookingId=this.newPassengerList[0].
-//       }
-//       )
-//     }
+    ngOnInit(){
 
-// // viewTicket(formdata:any){
-// //   const BookingId=formdata.BookingId;
-// //   this.ticketService.cancelTicket(BookingId).subscribe((data)=>{
-// //     this.ticket=data;
-// //     this.show=true
-// //   });
 
-// // }
 
-// //   change() {
-// //     this.router.navigate(['View-Ticket']);
-// //   }
+    
+      this.subs = this.bookingService.bookingDataSubject.subscribe(
+        (x) => (this.bookedBus = x)
+      );
+      
+     
 
-// }
+     
+    }
+
+
+}
