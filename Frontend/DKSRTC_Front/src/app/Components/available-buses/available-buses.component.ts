49f356e9a6b7f8b28161f598f285c2bfbe4723ed
AZURIIEAL <abinbinu678@gmail.com>
2023-09-17 11:08:43 +0530

    new

--- Frontend/DKSRTC_Front/src/app/Components/available-buses/available-buses.component.ts
+++ Frontend/DKSRTC_Front/src/app/Components/available-buses/available-buses.component.ts
@@ -11,38 +11,27 @@ import { AuthCheckService } from 'src/app/Services/auth-check.service';
   styleUrls: ['./available-buses.component.sass'],
 })
 export class AvailableBusesComponent implements OnInit {
-  // Constructor function to inject services
   constructor(
-    private authService: AuthCheckService, // AuthenticationService
-    private busRouteService: BusRouteService, // Service for fetching bus routes
-    private activatedRoute: ActivatedRoute // ActivatedRoute for route parameters
+    private authService: AuthCheckService,
+    private busRouteService: BusRouteService,
+    private activatedRoute: ActivatedRoute
   ) {}
 
-// Add this property to your component
-showFilters = false;
+  showFilters = false;
+  get filterButtonText() {
+    return this.showFilters ? 'Hide Filters' : 'Show Filters';
+  }
 
-// Function to toggle the filters
-toggleFilters() {
-  this.showFilters = !this.showFilters;
-}
-
-//Note this part -about getter function
-// Add this getter function to change the button text based on the filter state
-get filterButtonText() {
-  return this.showFilters ? 'Hide Filters' : 'Show Filters';
-}
-
-  // Declare class properties
-  availableBuses!: Observable<IBus[]>; // Observable to store the list of available buses
-  availableFilters: string[][] = []; // Initialize the availableFilters property
-  fromLocationId!: number; // Stores the ID of the departure location
-  isLoggedIn = false; // Stores the login status
-  fromLocationName!: string; // Stores the name of the departure location
-  toLocationId!: number; // Stores the ID of the destination location
-  toLocationName!: string; // Stores the name of the destination location
-  onDate!: Date; // Stores the date of the journey
-  selectedSpeederCommute: string = ''; // Initialize selectedSpeederCommute for filtering
-  selectedAccommodationClass: string = ''; // Initialize selectedAccommodationClass for filtering
+  availableBuses!: Observable<IBus[]>;
+  availableFilters: string[][] = [];
+  fromLocationId!: number;
+  isLoggedIn = false;
+  fromLocationName!: string;
+  toLocationId!: number;
+  toLocationName!: string;
+  onDate!: Date;
+  selectedSpeederCommute: string = '';
+  selectedAccommodationClass: string = '';
   keys = [
     'Bus Name',
     'Departure Time',
@@ -52,17 +41,32 @@ get filterButtonText() {
     'Seats Available',
     'Fare',
     'For Booking',
-  ]; // Array of keys for table headers
+  ];
 
-  // Function to display a login alert
+  noBusesFound = false;
+
   AlertLogin() {
     alert('Please login to view tickets or book tickets for your next journey');
   }
 
-  // ngOnInit() is called when the component is initialized
+  applyFilters() {
+    this.filteredBuses = this.availableBuses.pipe(
+      map((buses: IBus[]) => buses.filter((bus) => this.filterBus(bus)))
+    );
+
+    this.filteredBuses.subscribe((buses) => {
+      this.noBusesFound = buses.length === 0;
+    });
+  }
+
+  filteredBuses: Observable<IBus[]> | undefined;
+
+  toggleFilters() {
+    this.showFilters = !this.showFilters;
+  }
+
   ngOnInit() {
     this.activatedRoute.queryParamMap.subscribe((x) => {
-      // Extract and set route query parameters
       const fromLocationParam = x.get('fromLocation');
       if (fromLocationParam) {
         this.fromLocationName = fromLocationParam;
@@ -84,45 +88,40 @@ get filterButtonText() {
         this.onDate = new Date(journeyDateParam);
       }
 
-      // Get available buses based on route parameters
       this.availableBuses = this.busRouteService.getFilteredBuses(
         this.fromLocationId,
         this.toLocationId,
         this.onDate
       );
 
-      // Call the getAllFilters method from the service
-      const FiltercategoriesDropdown$ = this.busRouteService.getAllFilters()[0]; // Get the first observable(categories)
-      const FiltertypesDropdown$ = this.busRouteService.getAllFilters()[1]; // Get the second observable(types)
+      const FiltercategoriesDropdown$ = this.busRouteService.getAllFilters()[0];
+      const FiltertypesDropdown$ = this.busRouteService.getAllFilters()[1];
 
-      // Subscribe to both observables and merge their results
       forkJoin([FiltercategoriesDropdown$, FiltertypesDropdown$]).subscribe(
         (results: string[][]) => {
-          this.availableFilters = results; // Assign the result to availableFilters
-          console.log(this.availableFilters); // Log the availableFilters here
+          this.availableFilters = results;
         }
       );
 
-      // Filter the buses based on selected criteria using RxJS pipe and map
-      this.availableBuses = this.availableBuses.pipe(
+      this.filteredBuses = this.availableBuses.pipe(
         map((buses: IBus[]) => buses.filter((bus) => this.filterBus(bus)))
       );
 
-      // Check if the user is logged in
+      this.filteredBuses.subscribe((buses) => {
+        this.noBusesFound = buses.length === 0;
+      });
+
       this.authService.isLoggedIn().subscribe((status) => {
         this.isLoggedIn = status;
       });
     });
   }
 
-  // Function to filter buses based on selected criteria
   filterBus(bus: IBus): boolean {
-    // Check if both filters are empty (no filtering)
     if (!this.selectedSpeederCommute && !this.selectedAccommodationClass) {
       return true;
     }
 
-    // Check Speeder Commute filter
     if (
       this.selectedSpeederCommute &&
       bus.categoryName !== this.selectedSpeederCommute
@@ -130,7 +129,6 @@ get filterButtonText() {
       return false;
     }
 
-    // Check Accommodation Class filter
     if (
       this.selectedAccommodationClass &&
       bus.typeName !== this.selectedAccommodationClass
@@ -138,7 +136,6 @@ get filterButtonText() {
       return false;
     }
 
-    // If all checks pass, include the bus in the filtered list
     return true;
   }
 }
